e232df1f15fa2cede3e018958f51c44870042c0b
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-27 09:43:13 -0700

    Working on employee part of iteration 1

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,12 @@
+require 'date'
+
 class Employee
+  attr_reader :employee_id, :name, :role, :start_date, :end_date
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id.to_i
+    @name = name
+    @role = role
+    @start_date = DateHandler.string_to_date(start_date)
+    @end_date = DateHandler.string_to_date(end_date)
+  end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,10 +2,22 @@ require './test/test_helper'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
+  def setup
+    @employee = Employee.new(5, 'Sally Jackson', 'Engineer', '2015-01-01', '2018-01-01')
+  end
+
   def test_instantiation
-    employee = Employee.new
+    assert_instance_of Employee, @employee
   end
 
   def test_attributes
+    assert_equal 5, @employee.employee_id
+    assert_instance_of Integer, @employee.employee_id
+    assert_equal 'Sally Jackson', @employee.name
+    assert_equal 'Engineer', @employee.role
+    assert_equal '2015-01-01', @employee.start_date
+    assert_instance_of Date, @employee.start_date
+    assert_equal '2018-01-01', @employee.end_date
+    assert_instance_of Date, @employee.end_date
   end
 end
